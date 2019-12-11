1ae5e0540a2a8023600ab51c11b22091fb5c1df2
dB2510 <dhruvbodani2510@gmail.com>
2019-12-11 09:39:54 +0530

    optimised sieve

--- TDPRIMES.cpp
+++ TDPRIMES.cpp
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
-void normalSieve(int n) 
+// In optimised sieve we don't care about the even integers
+
+void optimisedSieve(int n) 
 { 
-	// prime[i] is going to store true if 
-	// if i*2 + 1 is composite. 
 	bool prime[n/2]; 
 	memset(prime, false, sizeof(prime)); 
 
-	// 2 is the only even prime so we can 
-	// ignore that. Loop starts from 3. 
-	for (int i=3 ; i*i < n; i+=2) 
+	// 2 is the only even prime so we can ignore that. Loop starts from 3. 
+	for(int i = 3 ; i*i < n; i += 2) 
 	{ 
-		// If i is prime, mark all its 
-		// multiples as composite 
+		// If i is prime, mark all its multiples as composite 
 		if (prime[i/2] == false) 
-			for (int j=i*i; j<n; j+=i*2) 
+			for(int j = i*i;j < n;j += i*2) 
 				prime[j/2] = true; 
 	} 
 
 	// writing 2 separately 
-	printf("2 "); 
-
+	std :: cout << 2 << std :: endl; 
+    long long int n1 = 1;
 	// Printing other primes 
-	for (int i=3; i<n ; i+=2) 
+	for(int i = 3; i < n ; i += 2) 
 		if (prime[i/2] == false) 
-			printf( "%d " , i ); 
+		{
+            n1++;
+            if(n1 % 100 == 1)
+            std :: cout << i << std :: endl;
+        } 
 } 
 
 // Driver code 
 int main() 
 { 
-	int n = 100 ; 
-	normalSieve(n); 
+	int n = 100000000; 
+	optimisedSieve(n); 
 	return 0; 
 } 
